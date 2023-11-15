c868cf2be139f923af37c00970d8071aa5116d59
Ritesh <gritesh493@gmail.com>
2023-11-16 01:10:58 +0530

    style : Hero Section Style updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,16 @@
-import HeroImg from "../assets/HeroImage_Food.jpg";
+import HeroImg from "../assets/Hero.jpg";
 const Hero = () => {
   return (
-    <div className="HeroComponentContainer h-80 mb-10 ">
+    <div className="HeroComponentContainer h-80 w-full mb-10 flex  items-center  sm:mt-20 lg:mt-0">
       <img
         src={HeroImg}
-        className=" w-full h-full object-cover overflow-hidden rounded-b-3xl "
+        className="pr-0 w-96 h-80 object-cover overflow-hidden rounded-b-3xl inline ml-[11%]"
       ></img>
-      <div className="absolute top-64 left-80 text-3xl font-bold text-white">
-        Order Food <br></br>Online in Kolkata
+      <div className=" flex justify-center items-center grow h-full p-2  text-3xl font-bold rounded-b-2xl bg-gradient-to-r from-white via-blue-50 to-slate-200">
+        <div className="p-2 mr-[33%] text-3xl font-extralight text-slate-600 rounded-2xl bg-gradient-to-r from-slate-100 via-blue-50">
+          Order Food <br></br>
+          <span className=" text-6xl ">Online </span>in Kolkata
+        </div>
       </div>
     </div>
   );
